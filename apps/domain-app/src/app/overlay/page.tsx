59512c62c815fce4327cae312c734a7ffae968fd
Zach Jaensch <zach.jaensch@safetyculture.io>
2025-01-14 16:46:26 +1100

    fix next app build

--- apps/domain-app/src/app/overlay/page.tsx
+++ apps/domain-app/src/app/overlay/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Home() {
+function Modal() {
   function handleClose() {
     window.parent.postMessage(
       { type: "modal", payload: { modal: undefined } },
@@ -11,21 +12,33 @@ export default function Home() {
   }
 
   const modal = useSearchParams().get("modal");
+
+  if (!modal) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="bg-black bg-opacity-30 absolute inset-0" />
+      <div className="h-[40rem] w-[40rem] bg-white rounded-lg p-4 text-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-row justify-between">
+          <div>Modal - {modal}</div>
+          <button className=" p-1 border-2" onClick={handleClose}>
+            Close
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default function Home() {
   return (
     <div className="relative">
-      {modal && (
-        <>
-          <div className="bg-black bg-opacity-30 absolute inset-0" />
-          <div className="h-[40rem] w-[40rem] bg-white rounded-lg p-4 text-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div className="flex flex-row justify-between">
-              <div>Modal - {modal}</div>
-              <button className=" p-1 border-2" onClick={handleClose}>
-                Close
-              </button>
-            </div>
-          </div>
-        </>
-      )}
+      {/* @ts-expect-error -- invalid ts config */}
+      <Suspense fallback={null}>
+        <Modal />
+      </Suspense>
     </div>
   );
 }
